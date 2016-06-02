a2adde8722cd9eda8662b90a107f8f445005509f
NielsBlaak <Nielsblaak@hotmail.com>
2016-06-02 10:48:11 +0200

    Bedrijven pagina

--- resources/views/pages/companies.blade.php
+++ resources/views/pages/companies.blade.php
@@ -6,6 +6,9 @@
 
 @section('content')
     <div class="title">Bedrijven</div>
+    <div class="col-md-4"> <h1>Bedrijf 1<br><small>Afbeelding</small></h1> </div>
+    <div class="col-md-4"> <h1>Bedrijf 2<br><small>Afbeelding</small></h1> </div>
+    <div class="col-md-4"> <h1>Bedrijf 3<br><small>Afbeelding</small></h1> </div>
 @stop
 
 @section('footer')
