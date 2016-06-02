5baa0d2df9a8cb557d192bd1da7714dacbb931bb
Dylan <Dylandegast@live.nl>
2016-06-02 13:24:55 +0200

    welkom pagina.

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -6,6 +6,11 @@ Welkom
 
 @section('content')
     <div class="title">Welkom</div>
+        <div class="stageblokjes">
+            <div class="leerbedrijf">(afbeelding)<br><br> leerbedrijf worden?</div>
+                <div class="stagiare">(afbeelding)<br><br> hoe vind ik een stagiare?</div>
+            <div class="zoekstage">(afbeelding) <br><br> zelf op zoek naar een stage?</div>
+        </div>
 @stop
 
 @section('footer')
