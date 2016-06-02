a7c9194c5fa0a1e044f03d53e3272c5fe4f17e2c
Veren <verenmanta@gmail.com>
2016-06-02 13:02:46 +0200

    Migrations Contacts, Stagetools, Companies & Tools aangemaakt.

---
+++ database/migrations/2016_06_02_105456_create_tools_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateToolsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tools', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('naam', 45)->nullable(false)->change();
+            $table->mediumText('omschrijving')->nullable(false)->change();
+            $table->unsignedInteger('status_id');
+            $table->foreign('status_id')->references('id')->on('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tools');
+    }
+}
