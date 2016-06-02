0ca3bc7404299c4356b434a9455f35249fb1f35f
Veren <verenmanta@gmail.com>
2016-06-02 15:02:53 +0200

    e-mail vergeten bij user table

--- database/migrations/2016_06_02_000002_create_users_table.php
+++ database/migrations/2016_06_02_000002_create_users_table.php
@@ -14,6 +14,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('email', 250)->unique();
             $table->string('password');
             $table->unsignedInteger('role_id');
             $table->foreign('role_id')->references('id')->on('roles');
