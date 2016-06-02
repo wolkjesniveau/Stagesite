d3aecfb11e368156bd72077a76ef7a10a2830d94
NielsBlaak <Nielsblaak@hotmail.com>
2016-06-02 13:32:02 +0200

    User seeder aangemaakt

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Henk vd Berg',
+            'email' => 'HenkvdBerg@onzin.nl',
+            'password' => bcrypt('password'),
+            'role_id' => 1
+
+        ]);
+    }
+}
