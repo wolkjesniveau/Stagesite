c6e0dc48a9e0d382b8fcff313cbd6ce2fe698a6c
NielsBlaak <Nielsblaak@hotmail.com>
2016-06-02 12:44:10 +0200

    Verschillende migrations & models aangemaakt

---
+++ app/Study.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Study extends Model
+{
+    protected $fillable = [
+        'education_id'
+    ];
+}
---
+++ database/migrations/2016_06_02_000006_create_cohorts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCohortsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cohorts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('schoolyear');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('cohorts');
+    }
+}
