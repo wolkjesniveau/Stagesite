927311b34b48f2cedac099f6e4eaae74a71f66bf
Veren <verenmanta@gmail.com>
2016-06-14 09:50:19 +0200

    FIXED seeders, eindelijk werken die pokke dingen

--- database/seeds/StatusTableSeeder.php
+++ database/seeds/StatusTableSeeder.php
@@ -11,7 +11,7 @@ class StatusTableSeeder extends Seeder
      */
     public function run()
     {
-        \app\Status::create([
+        \App\Status::create([
             'status' => 'lit'
         ]);
     }
