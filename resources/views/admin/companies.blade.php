245d31877045be9cc940e5d1b9a87580a59238ac
NielsBlaak <Nielsblaak@hotmail.com>
2016-06-14 09:02:45 +0200

    Admin Companies

---
+++ resources/views/admin/companies.blade.php
@@ -0,0 +1,16 @@
+@extends('master')
+
+@section('title')
+    Admin Bedrijven overzicht
+@stop
+
+@section('content')
+
+    <h1>Admin bedrijven pagina</h1>
+
+
+@stop
+
+@section('footer')
+    <p>footer</p>
+@stop
