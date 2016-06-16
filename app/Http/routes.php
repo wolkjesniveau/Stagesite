325829ba3fe35396f5cb8ef211a9a52f9e4e6472
karay karayy <karayzakelijk@live.nl>
2016-06-16 14:07:18 +0200

    Merge branch 'develop' of C:\wamp\www\Stagesite with conflicts.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,47 +13,20 @@
 
 
 Route::get('/', 'PagesController@welcome')->name('welcome');
-//Route::get('companies', 'PagesController@companies')->name('companies');
+Route::get('companies', 'PagesController@companies')->name('companies');
 Route::get('company', 'PagesController@company')->name('company');
 Route::get('master', 'PagesController@master')->name('master');
 Route::get('register', 'PagesController@register')->name('register');
 // ROLE CHECK MUST BE ADDED HERE WHEN DATABASE IS CORRECT
-Route::get('admin', 'PagesController@admin')->name('admin');
-Route::get('admin/internshipAdmin', 'PagesController@internshipAdmin')->name('internshipAdmin');
-Route::get('admin/studiesAdmin', 'PagesController@studiesAdmin')->name('studiesAdmin');
-Route::get('admin/schoolsAdmin', 'PagesController@schoolsAdmin')->name('schoolsAdmin');
-Route::get('admin/companiesAdmin', 'PagesController@companiesAdmin')->name('companiesAdmin');
 Route::get('admin', 'AdminController@admin')->name('admin');
 Route::get('internshipAdmin', 'AdminController@internshipAdmin')->name('internshipAdmin');
 Route::get('studiesAdmin', 'AdminController@studiesAdmin')->name('studiesAdmin');
 Route::get('schoolsAdmin', 'AdminController@schoolsAdmin')->name('schoolsAdmin');
 Route::get('companiesAdmin', 'AdminController@companiesAdmin')->name('companiesAdmin');
-
-/* TOOLS SECTION */
-Route::get('toolsIndex', 'ToolsController@Index')->name('toolsIndex');
-Route::get('toolsEdit/{id}', 'ToolsController@Edit')->name('tools-edit');
-Route::post('tool/{id}', 'ToolsController@update')->name('tool.edit');
-Route::get('toolsCreate', 'ToolsController@Create')->name('tools.create');
-Route::get('toolsShow/{id}', 'ToolsController@Show')->name('tools-show');
-
-//Company Routes
-Route::post('companies', 'CompaniesController@store');
-Route::get('companies/create', 'CompaniesController@create');
-Route::get('companies/{id}/edit', 'CompaniesController@edit');
-Route::put('companies','CompaniesController@update');
-Route::delete('companies/{id}','CompaniesController@destroy');
-Route::resource('companies', 'CompaniesController', ['only'=>['index','create','store']]);
+Route::get('toolsAdmin', 'AdminController@toolsAdmin')->name('toolsAdmin');
+Route::get('usersAdmin', 'AdminController@usersAdmin')->name('usersAdmin');
 
 
-Route::get('tool/edit/{id}', array('as' => 'tool.edit', function($id)
-{
-    // return our view and tool information
-    return View::make('admin.toolUpdate') // pulls app/views/toolUpdate.blade.php
-    ->with('tool', \App\Tool::find($id));
-}));
-Route::post('tool/edit', function() {
-    // process our form
-});
 
 // Model Routes...
 Route::get('internships', 'InternshipsController@index');
--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -3,7 +3,6 @@
 @section('content')
     <li><a href="{{ route('internshipAdmin') }}">Overzicht van Stages</a></li>
     <li><a href="{{ route('companiesAdmin') }}">Overzicht van bedrijven</a></li>
-    <li><a href="{{ route('schoolsAdmin') }}">Overzicht van scholen</a></li>
     <li><a href="{{ route('studiesAdmin') }}">Overzicht van Opleidingen</a></li>
-    <li><a href="{{ route('toolsIndex') }}">Overzicht van Tools</a></li>
+    <li><a href="{{ route('toolsAdmin') }}">Overzicht van Tools</a></li>
 @endsection
