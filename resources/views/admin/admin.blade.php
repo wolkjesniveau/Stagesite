133f067e7b90d43bb4aab56498a280798cdca7ed
Veren <verenmanta@gmail.com>
2016-06-17 12:10:20 +0200

    Fixed the layouts

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <li><a href="{{ route('internshipAdmin') }}">Overzicht van Stages</a></li>
-    <li><a href="{{ route('companiesAdmin') }}">Overzicht van bedrijven</a></li>
+    <li><a href="{{ route('companies.index') }}">Overzicht van bedrijven</a></li>
     <li><a href="{{ route('schoolsAdmin') }}">Overzicht van scholen</a></li>
     <li><a href="{{ route('studies.index') }}">Overzicht van Opleidingen</a></li>
     <li><a href="{{ route('tools.index') }}">Overzicht van Tools</a></li>
